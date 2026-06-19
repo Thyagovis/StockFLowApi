5be5511c334f2408fff5313865361ddbd4d31943
Miguel Arcanjo <migueljnp707@gmail.com>
2026-06-19 00:42:40 -0300

    Refatorações em Categoria

---
+++ src/main/java/com/stockflow/StockFlowApi/categoria/dto/CategoriaMapper.java
@@ -0,0 +1,21 @@
+package com.stockflow.StockFlowApi.categoria.dto;
+
+import com.stockflow.StockFlowApi.categoria.entity.Categoria;
+
+public class CategoriaMapper {
+
+    public static CategoriaResponseDTO toResponseDTO(Categoria categoria) {
+        if (categoria == null) {
+            return null;
+        }
+
+        return new CategoriaResponseDTO(
+                categoria.getId(),
+                categoria.getNome(),
+                categoria.getDescricao(),
+                categoria.getDataCadastro(),
+                categoria.isAtivo()
+        );
+    }
+
+}
